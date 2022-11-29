3ff6b998af58744e2a48ed3ac97bb6b84b1d23f8
Tirth <tirthsolanki29923@gmail.com>
2022-11-29 22:17:33 +0530

    access modifires

---
+++ Section-13/7_accessmodifires.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class player {
+private: 
+    //attribute
+        string name {"player"};
+        int health {100};
+        int xp {5};
+public:
+    //methods
+    void talk(string text_to_say) {cout<<name<<" says "<<text_to_say<<endl;}
+    bool is_dead();
+};
+
+int main()
+{
+    player frank;
+    // frank.name = "Frank";
+    // frank.health=49;
+
+    frank.talk("Hello");
+
+
+    return 0;
+}
