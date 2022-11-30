dbf0a65777e6226db990991fa7b9a3f6594f14cc
Tirth <tirthsolanki29923@gmail.com>
2022-11-30 11:01:07 +0530

    implimenting_method

--- Section-13/8_implimenting_method.cpp
+++ Section-13/8_implimenting_method.cpp
@@ -2,27 +2,74 @@
 #include <string>
 using namespace std;
 
-
-class player {
-public:
-    // attributes
-    string name;
-    int health;
-    int xp;
-    
-    // methods
-    void talk(string text_to_say) { cout << name << " says " << text_to_say << endl;  }
-    bool is_dead();
-};
-
-class account {
-public:
+class Account
+{
+private:
     // attributes
     string name;
     double balance;
-    
+
+public:
     // methods
-    bool deposit(double bal) {balance += bal; cout << "In deposit" << endl; }
-    bool withdraw(double bal) {balance -= bal; cout << "In withdraw" << endl;  }
-    
+    // declared inline
+    void set_balance(double bal) { balance = bal; }
+    double get_balance() { return balance; }
+
+    // methods will be declared outside the class declaration
+    void set_name(string n);
+    string get_name();
+
+    bool deposit(double amount);
+    bool withdraw(double amount);
 };
+
+void Account::set_name(string n)
+{
+    name = n;
+}
+
+string Account::get_name()
+{
+    return name;
+}
+
+bool Account::deposit(double amount)
+{
+    balance += amount;
+    return true;
+}
+
+bool Account::withdraw(double amount)
+{
+    if (balance - amount > 0)
+    {
+        balance -= amount;
+        return true;
+    }
+    else
+        return false;
+}
+
+int main()
+{
+    Account Tirth_account;
+    Tirth_account.set_name("Tirth's account");
+    Tirth_account.set_balance(1000.0);
+
+    if (Tirth_account.deposit(200.0))
+        cout << "Deposit OK" << endl;
+    else
+        cout << "Deposit Not allowed" << endl;
+
+    if (Tirth_account.withdraw(500.0))
+        cout << "Withdrawal OK" << endl;
+    else
+        cout << "Not sufficient funds" << endl;
+
+    if (Tirth_account.withdraw(1500.0))
+        cout << "Withdraw OK" << endl;
+    else
+        cout << "Not sufficient funds" << endl;
+
+    return 0;
+}
