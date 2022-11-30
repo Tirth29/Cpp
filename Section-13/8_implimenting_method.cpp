0a0e5ef841586f3108c881a3f9685daf8ca527c8
Tirth <tirthsolanki29923@gmail.com>
2022-11-30 10:43:21 +0530

    add

--- Section-13/8_implimenting_method.cpp
+++ Section-13/8_implimenting_method.cpp
@@ -2,3 +2,27 @@
 #include <string>
 using namespace std;
 
+
+class player {
+public:
+    // attributes
+    string name;
+    int health;
+    int xp;
+    
+    // methods
+    void talk(string text_to_say) { cout << name << " says " << text_to_say << endl;  }
+    bool is_dead();
+};
+
+class account {
+public:
+    // attributes
+    string name;
+    double balance;
+    
+    // methods
+    bool deposit(double bal) {balance += bal; cout << "In deposit" << endl; }
+    bool withdraw(double bal) {balance -= bal; cout << "In withdraw" << endl;  }
+    
+};
